9113b1f605e9a3cc920a6d1317a4cd713d01d44c
johnpharmd <jhumphre@uw.edu>
2018-10-07 13:04:29 -0700

    discovered that was using too much integration function logic in threading_scrape, and that worker() should likely call get_articles; refactored threading_scrape with only a thread_count param; refactored worker

--- students/johnpharmd/lesson09/threaded_newsapi_scraper.py
+++ students/johnpharmd/lesson09/threaded_newsapi_scraper.py
@@ -2,14 +2,12 @@
 
 import threading
 import queue
-# import functools
 import time
 import requests
 
+
 MAX_THREAD_POOL = 250
-
 WORD = 'trump'
-
 NEWS_API_KEY = '0b7b85e7886047c6a516f5b5516276a1'
 
 base_url = 'https://newsapi.org/v1/'
@@ -56,45 +54,6 @@ def get_articles(source):
     return titles
 
 
-def scrape_f(f, N):
-    # dx = (b - a) / N
-    return sum(map(f, (i for i in range(N))))
-
-
-# def timer(func):
-#     """Print the runtime of the decorated function"""
-#     # from https://realpython.com/primer-on-python-decorators/
-#     @functools.wraps(func)
-#     def wrapper_timer(*args, **kwargs):
-#         start_time = time.perf_counter()    # 1
-#         value = func(*args, **kwargs)
-#         end_time = time.perf_counter()      # 2
-#         run_time = end_time - start_time    # 3
-#         print(f'Finished {func.__name__!r} in {run_time:.4f} secs')
-#         return value
-#     return wrapper_timer
-
-
-# @timer
-def threading_api(func, N, thread_count=2):
-    """Scrapes newsAPI via multiple threads"""
-    # break work into N chunks
-    # see https://uwpce-pythoncert.github.io/PythonCertDevel/modules/ThreadingMultiprocessing.html
-    N_chunk = int(float(N) / thread_count)
-    results = queue.Queue()
-
-    def worker(*args):
-        results.put(scrape_f(*args))
-
-    if thread_count < MAX_THREAD_POOL:
-        for i in range(thread_count):
-            thread = threading.Thread(target=worker, args=(func, N_chunk))
-            thread.start()
-            print("Thread %s started" % thread.name)
-
-    return sum((results.get() for i in range(thread_count)))
-
-
 def count_word(word, titles):
     word = word.lower()
     count = 0
@@ -104,22 +63,44 @@ def count_word(word, titles):
     return count
 
 
+def threading_scrape(thread_count=2):
+    """Scrapes newsAPI via multiple threads"""
+    # see https://uwpce-pythoncert.github.io/PythonCertDevel/modules/ThreadingMultiprocessing.html
+    results = queue.Queue()
+    sources = get_sources()
+    thread_pool = 0
+
+    def worker():
+        art_count = 0
+        word_count = 0
+        for source in sources:
+            titles = get_articles(source)
+            art_count += len(titles)
+            word_count += count_word('trump', titles)
+
+        results.put((word_count, art_count))
+
+    for i in range(thread_count):
+        if thread_pool < MAX_THREAD_POOL:
+            thread = threading.Thread(target=worker)
+            thread.start()
+            thread_pool += 1
+            print("Thread %s started" % thread.name)
+
+    return sum((results.get() for i in range(thread_count)))
+
+
 if __name__ == "__main__":
 
-    # scraping parameters
-    N = 10**7
-    thread_count = 2
+    # scraping parameter
+    thread_count = 4
 
     start = time.time()
-    # sources = threading_api(get_sources(), N, thread_count)
-    # titles = threading_api(get_articles(source), N, thread_count)
-    art_count = 0
-    word_count = 0
-    for source in sources:
-        titles = get_articles(source)
-        art_count += len(titles)
-        word_count += count_word('trump', titles)
-    print(WORD, f'found {word_count} times in {art_count} articles,' +
-                ' using {N} chunks')
 
+    try:
+        word_count, art_count = threading_scrape(thread_count)
+    except Exception as e:
+        print(f'Error occurred: {e}')
+
+    print(WORD, f'found {word_count} times in {art_count} articles')
     print('Process took {:.0f} seconds'.format(time.time() - start))
